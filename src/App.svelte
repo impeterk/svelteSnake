ffe5681bd8854869d00672dd29045a8af09e14fb
peter@debian <peterkudelas@gmail.com>
2023-04-07 23:23:29 +0200

    added tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,21 @@
 <script lang="ts">
+  import Canvas from "./Canvas.svelte";
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
-  <div>
+<main class="flex flex-col items-center">
+  <div class="flex w-full justify-around">
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      <img src={viteLogo} class="aspect-square h-28" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img src={svelteLogo} class="aspect-square h-28" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check this out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Add stuff Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1 class="m-8 font-bold text-5xl text-gray-200">Vite + Svelte</h1>
+  <Canvas />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/Canvas.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+</script>
+
+<canvas width="600px" height="600px" class="border-8 border-indigo-600" />
