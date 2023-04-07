9ed5419304f7918a51c15892bbcb41dd70b35a17
peter@debian <peterkudelas@gmail.com>
2023-04-07 23:34:03 +0200

    added canvas

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   const canvas = document.querySelector("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  ctx.fillStyle = "#9ca3af";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 </script>
 
 <canvas width="600px" height="600px" class="border-8 border-indigo-600" />
